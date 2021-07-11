22ec08f6ec765516df39350eaeb6aaa8b8d7814e
Forbiddenman <86903837+Forbiddenman@users.noreply.github.com>
2021-07-11 11:41:12 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"{file_name} \n Powered by:MOVIEZZ 4U",
+                caption = f" Powered by:MOVIEZZ 4U",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
